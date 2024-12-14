c9b92df1552bd52b781c2ddbda53307a88007539
Aleko Georgiev <algugone@gmail.com>
2024-12-15 01:52:36 +0200

    some good, some bad, some ugly

--- seminar-and-practice-tasks/week05/seminar.cpp
+++ seminar-and-practice-tasks/week05/seminar.cpp
@@ -2,6 +2,27 @@
 #include <cstring>
 #include <cmath>
 
+int strlen_rec(const char str[], int i) {
+    if (!str[i]) {
+        return i;
+    }
+    return strlen_rec(str, i+1);
+}
+
+void shift_characters(char str[], int start_idx, int positions) {
+    int i = start_idx;
+    int j = start_idx + positions;
+    while (str[i] && str[j]) {
+        str[i] = str[j];
+        ++i;
+        ++j;
+    }
+
+    if (str[i] && str[j] == '\0') {
+        str[i] = str[j];
+    }
+}
+
 bool is_numeric(char c) {
     return c >= '0' && c <= '9';
 }
@@ -82,12 +103,73 @@ void decode(char destination[], const char source[]) {
     int i = 0;
     int j = 0;
     while (source[i] && source[i + 1]) {
-        int num_index = i + 1;
-        while(is_numeric(source[num_index])) {
-
+        int number = 0;
+        while (is_numeric(source[i])) {
+            number = number * 10 + (int) (source[i] - '0');
+            ++i;
         }
-        i += num_index;
+        if (number == 0) {
+            destination[j++] = source[i++];
+        } else {
+            for (int k = 0; k < number; ++k) {
+                destination[j++] = source[i];
+            }
+            ++i;
+        }
     }
+}
+
+int find_word(const char str[], const char word[]) {
+    int i = 0, j = 0;
+    int start = 0;
+    while (str[i]) {
+        if (word[j] == '\0') {
+            return start;
+        }
+
+        if (str[i] == word[j]) {
+            if (j == 0) {
+                start = i;
+            }
+            j++;
+        } else if (str[i] != word[j]) {
+            start = j = 0;
+        }
+        ++i;
+    }
+
+    return -1;
+}
+
+void remove_word(char str[], const char word[]) {
+    int index  = find_word(str, word);
+    shift_characters(str, index, strlen_rec(word, 0)+1);
+}
+
+void reverse_sentence(char destination[], const char source[]) {
+    int i = 0, size = 0;
+    int dest_i = 0;
+    while (source[i]) {
+        ++i;
+    }
+    --i;
+    while (i >= 0) {
+        while (source[i] != ' ' && i >= 0) {
+            --i;
+            size++;
+        }
+        for (int j = i+1; j < i+size+1; j++) {
+            destination[dest_i++] = source[j];
+        }
+        destination[dest_i++] = ' ';
+        --i;
+        size = 0;
+    }
+    destination[dest_i++] = '\0'; 
+}
+
+void replace_word(char str[], const char word1[], const char word2[]) {
+    
 }
 
 int main() {
@@ -108,5 +190,27 @@ int main() {
     // std::cout << str << '\n'; // -> Hello World
 
     // task 5
+    // char buffer[255];
+    // decode(buffer, "a2b10c");
+    // std::cout << buffer << '\n'; // -> abbcccccccccc
+
+    // task 6
+    // std::cout << find_word("I am the best programmer ever", "programmer") << '\n'; // -> 14
+
+    // task 7
+    // char str[] = "I am the best programmer ever";
+    // remove_word(str, "programmer");
+    // std::cout << str << '\n'; // -> I am the best ever
+
+    // task 8
+    // char buffer[255];
+    // reverse_sentence(buffer, "May the force be with you");
+    // std::cout << buffer << '\n'; // -> you with be force the May
+
+    // task 9
+    char str[] = "I am the best programmer ever";
+    replace_word(str, "programmer", "boxer");
+    std::cout << str << '\n'; // -> I am the best boxer ever
+
     return 0;
 }
